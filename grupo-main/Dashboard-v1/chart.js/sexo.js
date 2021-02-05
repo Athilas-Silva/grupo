e96418b71cc94320796902d539bb44dbe5e7d02f
Ana Carolina Oliveira <67801949+AnaCarolinaOliveira23@users.noreply.github.com>
2021-02-05 09:17:41 -0300

    Add files via upload

---
+++ grupo-main/Dashboard-v1/chart.js/sexo.js
@@ -0,0 +1,20 @@
+
+var ctx = document.getElementById('chart2').getContext('2d');
+var chart = new Chart(ctx, {
+// The type of chart we want to create
+type: 'pie',
+
+// The data for our dataset
+data: {
+    labels: ['Feminino', 'Masculino', 'Outras'],
+    datasets: [{
+        label: 'Nível de estresse',
+        backgroundColor: ['rgb(255, 99, 132)', 'blue', 'yellow'],
+        borderColor: 'rgb(255, 99, 132)',
+        data: [1100, 650, 435]
+    }]
+},
+
+// Configuration options go here
+options: {}
+});
